c83c6fa48ca851ec4481466a3ab87db33be32c2b
Eriberto Lopez <eribertolopez3@gmail.com>
2020-05-19 21:46:12 -0700

    configure get and create Todo

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/http/createTodo.ts
@@ -3,10 +3,24 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyHandler, APIGatewayProxyResult } from 'aws-lambda'
 
 import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils'
+import { createLogger } from '../../utils/logger'
+import { createTodo } from '../../logicallayer/todo'
 
+
+const logger = createLogger('ToDo')
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const newTodo: CreateTodoRequest = JSON.parse(event.body)
+  const user_id = getUserId(event)
+  const todoItem = await createTodo(newTodo, user_id)
 
-  // TODO: Implement creating a new TODO item
-  return undefined
+
+  return {
+    statusCode: 201,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({item: todoItem})
+  }
 }
