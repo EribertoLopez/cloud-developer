edb6120cb36febf4015e6fb1b7f8524d49940cbe
Eriberto Lopez <eribertolopez3@gmail.com>
2020-05-20 20:19:05 -0700

    conrfiguring Auto0 application integration

--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -3,8 +3,7 @@ const apiId = '...'
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'udacity-cloud-developer.auth0.com',            // Auth0 domain
+  clientId: 'DNNbawOsDDL16zidFOFOkqi0mInYlQHi',          // Auth0 client id
   callbackUrl: 'http://localhost:3000/callback'
 }
